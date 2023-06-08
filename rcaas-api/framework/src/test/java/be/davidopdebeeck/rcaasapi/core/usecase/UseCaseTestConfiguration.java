79c8fc34f17c6e7a7523cfaa97c118da1289faa4
David Op de Beeck <david.opdebeeck@cegeka.com>
2023-06-08 20:15:34 +0200

    remove duplicate tests in framework module

--- rcaas-api/framework/src/test/java/be/davidopdebeeck/rcaasapi/core/usecase/UseCaseTestConfiguration.java
+++
@@ -1,9 +0,0 @@
-package be.davidopdebeeck.rcaasapi.core.usecase;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan
-public class UseCaseTestConfiguration {
-}
