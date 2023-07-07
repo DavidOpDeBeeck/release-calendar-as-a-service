f9c0d1707e9883ad27c37962173a0cbb08e28a83
David Op de Beeck <davidopdebeeck@outlook.be>
2023-07-07 15:42:09 +0200

    update hero page styling

--- rcaas-ui/src/pages/HeroPage.tsx
+++ rcaas-ui/src/pages/HeroPage.tsx
@@ -9,11 +9,13 @@ export default function HeroPage() {
     return (
         <div className="flex h-screen w-screen flex-col items-center justify-center bg-gray-100 p-8 dark:bg-slate-800">
             <div className="mt-4 mb-8 flex flex-none flex-col text-center">
-                <h1 className="text-5xl font-semibold text-gray-800 dark:text-gray-200">Release Calendar As a Service</h1>
-                <p className="mt-3 text-xl text-gray-800 dark:text-gray-200">Create, manage and share your release calendar with your team and your clients.</p>
-            </div>
-            <div className="flex sm:w-100 md:w-[550px]">
-                <CreateProject/>
+                <h1 className="text-5xl font-semibold text-gray-800 dark:text-slate-200">Release Calendar As a Service</h1>
+                <p className="mt-3 text-xl text-gray-800 dark:text-slate-300">
+                    Create, manage and share your release calendar with your team and your clients.
+                </p>
+                <div className="mt-8">
+                    <CreateProject/>
+                </div>
             </div>
         </div>
     )
--- rcaas-ui/src/project/CreateProject.tsx
+++ rcaas-ui/src/project/CreateProject.tsx
@@ -16,19 +16,16 @@ export default function CreateProject() {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}
-              className="flex flex-1 flex-col rounded-lg border border-gray-200 bg-white p-3 text-xl shadow-md space-y-2 dark:border-slate-800 dark:bg-slate-700">
-            <div className="flex items-center space-x-2">
-                <div className="flex flex-1 items-center">
-                    <div className="relative w-full">
-                        <input id="name"
-                               type="text"
-                               placeholder="Project Name"
-                               {...register("name", {required: true})}
-                               className="w-full rounded-lg p-3 text-gray-800 focus:outline focus:outline-2 focus:outline-blue-600 dark:bg-slate-700 dark:text-gray-200"/>
-                    </div>
-                </div>
+              className="flex rounded-lg border border-gray-200 bg-white p-3 text-xl shadow-md space-y-2 dark:border-slate-800 dark:bg-slate-700">
+            <div className="flex flex-1 items-center space-x-3">
+                <input id="name"
+                       type="text"
+                       placeholder="Project Name"
+                       {...register("name", {required: true})}
+                       autoComplete="off"
+                       className="autofill:bg-slate-700 flex-1 rounded-lg p-3 text-gray-800 focus:outline focus:outline-2 focus:outline-blue-600 dark:bg-slate-700 dark:text-slate-200"/>
                 <button type="submit"
-                        className="rounded-lg border-2 border-slate-800 px-4 py-3 font-semibold text-gray-800 hover:bg-gray-50 focus:outline focus:outline-2 focus:outline-blue-600 dark:text-gray-200 dark:hover:bg-slate-600">
+                        className="rounded-lg px-4 py-3 font-semibold text-gray-800 hover:bg-gray-50 outline outline-2 outline-gray-200 dark:outline-slate-800 focus:outline-blue-600 dark:text-gray-200 dark:hover:bg-slate-600">
                     Create
                 </button>
             </div>
