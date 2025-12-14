54d96dc26cea66ef591dcee5c4d387bb7a09c23e
David Op de Beeck <davidopdebeeck@outlook.be>
2025-12-14 20:23:23 +0100

    Use new SMD version

--- rcaas-api/build.gradle.kts
+++ rcaas-api/build.gradle.kts
@@ -26,13 +26,13 @@ subprojects {
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
         constraints {
-            implementation("app.dodb:smd-api:0.0.3")
-            implementation("app.dodb:smd-spring-boot-starter:0.0.3")
+            implementation("app.dodb:smd-api:0.0.5")
+            implementation("app.dodb:smd-spring-boot-starter:0.0.5")
             implementation("org.springframework.boot:spring-boot-dependencies:3.5.6")
             implementation("org.apache.commons:commons-lang3:3.19.0")
             implementation("com.fasterxml.jackson.core:jackson-databind:2.20.0")
             implementation("com.fasterxml.jackson.core:jackson-annotations:2.20")
-            testImplementation("app.dodb:smd-spring-boot-starter-test:0.0.3")
+            testImplementation("app.dodb:smd-spring-boot-starter-test:0.0.5")
             testImplementation("org.testcontainers:postgresql:1.21.3")
             testImplementation("org.testcontainers:junit-jupiter:1.21.3")
         }
